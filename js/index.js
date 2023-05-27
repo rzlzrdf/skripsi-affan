ba1213403b92442df1e048dd7f6611a9c87ba153
Rizal Lazuardi Firdaus <rizal.sekawanmedia@gmail.com>
2023-05-27 19:31:40 +0700

    adjust slider, adjust login page

--- js/index.js
+++ js/index.js
@@ -9,3 +9,27 @@ function myFunction() {
         x.type = "password";
     }
 }
+
+// swiper.js for carousel
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    slidesPerView: 10,
+    spaceBetween: 80,
+    centeredSlides: false,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 0,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+});
+
